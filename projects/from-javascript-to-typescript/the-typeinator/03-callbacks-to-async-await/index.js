ab1fb00658bef421333282139e8ba832ae716512
Jan Markowski <jan@markowski.ca>
2023-11-10 17:00:16 -0500

    Complete 01: From JavaScript to TypeScript - Callbacks to Async Await

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,21 @@
-// Put your checkEmotion and speak functions here! ✨
-// See ./original.js for their older JavaScript code.
+async function checkEmotion(knownEmotions, emotion) {
+	// Simulate database processing time by waiting a second...
+	await new Promise((resolve) => {
+		return setTimeout(resolve, 1000);
+	});
+
+	return knownEmotions.has(emotion);
+}
+
+async function speak(knownEmotions, newEmotion, phrase, callback) {
+	let hasEmotion = await checkEmotion(knownEmotions, newEmotion);
+
+	if (!hasEmotion) {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+
+	return `"${phrase}" (${newEmotion})`;
+}
 
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
